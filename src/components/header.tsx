3b862449f149a08c0070b7375d539690df30de83
on <nogiglee@gmail.com>
2023-07-15 11:25:00 +0900

    HOMEページの要素記載

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>これやろう</h1>
+            <nav>
+                <ul>
+                    <li>HOME</li>
+                    <li>PAGE1</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
---
+++ src/components/task.tsx
@@ -0,0 +1,9 @@
+export default function Task() {
+    return (
+        <div>
+            <h3>タスク名</h3>
+            <p>期限</p>
+            <a href="">詳細</a>
+        </div>
+    );
+}
